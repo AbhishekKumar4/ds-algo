f53ee59910cfd2efef804eb0c2ce5decd382fae8
Abhishek <kumarabhishek0388@gmail.com>
2023-07-04 17:43:26 +0530

    Palindrome

---
+++ src/com/abhishek/recursion/Palindrome.java
@@ -0,0 +1,22 @@
+package com.abhishek.practice.recursion;
+
+public class Palindrome {
+
+    public boolean checkPalindrome(int l, int r, String input) {
+       //base case
+       if(input.charAt(l) != input.charAt(r)) {
+           return false;
+       }
+
+       // recursive case
+        while(l<r) {
+            return checkPalindrome(l+1, r-1, input);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "Abhi";
+        System.out.println(new Palindrome().checkPalindrome(0, input.length()- 1, input));
+    }
+}
