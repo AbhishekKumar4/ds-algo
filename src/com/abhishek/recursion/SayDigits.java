28831ee076c62b23dbf8e45e379ba3e23f6c1c95
Abhishek <kumarabhishek0388@gmail.com>
2023-07-11 12:17:30 +0530

    Say Digits

---
+++ src/com/abhishek/recursion/SayDigits.java
@@ -0,0 +1,24 @@
+package com.abhishek.practice.recursion;
+
+public class SayDigits {
+
+    String[] dictionary = {"zero", "one", "two", "three", "four", "five"
+                            , "six", "seven", "eight", "nine", "ten"};
+    public void sayDigits(int input) {
+        // base case
+        if(input == 0) {
+            return;
+        }
+        int digit = input % 10;
+        input = input / 10;
+
+        // recursive call
+        sayDigits(input);
+        System.out.println(dictionary[digit]);
+
+    }
+
+    public static void main(String[] args) {
+        new SayDigits().sayDigits(1234);
+    }
+}
