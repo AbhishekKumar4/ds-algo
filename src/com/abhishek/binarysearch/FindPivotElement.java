ca256faf9c0cda49ccf12fcbc17a71340cc8901e
Abhishek Kumar <kumarabhishek0388@gmail.com>
2023-07-24 10:16:45 +0530

    refactor code

---
+++ src/com/abhishek/binarysearch/FindPivotElement.java
@@ -0,0 +1,24 @@
+package com.abhishek.binarysearch;
+
+public class FindPivotElement {
+
+    public int findPivot(int[] input) {
+        int start = 0;
+        int end = input.length;
+
+        while(start < end) {
+            int mid = start + (end - start)/2;
+            if(input[mid] >= input[0]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] input = { 4, 6, 8, 9, 1, 2, 3 };
+        System.out.println(new FindPivotElement().findPivot(input));
+    }
+}
--- src/com/abhishek/binarysearch/MyBinarySearchTree.java
+++ src/com/abhishek/binarysearch/MyBinarySearchTree.java
@@ -1,4 +1,4 @@
-package com.abhishek.ds;
+package com.abhishek.binarysearch;
 
 import java.util.ArrayList;
 import java.util.List;
