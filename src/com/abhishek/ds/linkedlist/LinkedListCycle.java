5dfb33f2d58db7559761f83d238fa852c1294b1d
Abhishek Kumar <kumarabhishek0388@gmail.com>
2023-11-17 13:22:11 +0530

    floyds cycle finding algo - loop

---
+++ src/com/abhishek/ds/linkedlist/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package com.abhishek.ds.linkedlist;
+
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+      }
+  }
+    public boolean hasCycle(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while(fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if(slowPointer ==  fastPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/com/abhishek/ds/linkedlist/MiddleOfTheLinkedList.java
@@ -0,0 +1,4 @@
+package com.abhishek.ds.linkedlist;
+
+public class MiddleOfTheLinkedList {
+}
