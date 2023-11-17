4783b9acfae92477eedbd306c9c16ec4a0718040
Abhishek Kumar <kumarabhishek0388@gmail.com>
2023-11-17 13:26:10 +0530

    find middle of the linkedlist

--- src/com/abhishek/ds/linkedlist/MiddleOfTheLinkedList.java
+++ src/com/abhishek/ds/linkedlist/MiddleOfTheLinkedList.java
@@ -1,4 +1,16 @@
 package com.abhishek.ds.linkedlist;
 
 public class MiddleOfTheLinkedList {
+
+    public LinkedListCycle.ListNode middleNode(LinkedListCycle.ListNode head) {
+        LinkedListCycle.ListNode slowPointer = head;
+        LinkedListCycle.ListNode fastPointer =  head;
+
+        while(fastPointer != null && fastPointer.next != null) {
+            fastPointer =  fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+
+        return slowPointer;
+    }
 }
